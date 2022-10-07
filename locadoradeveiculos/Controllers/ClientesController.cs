e7b374dd39e5aaa2ad20b83758e1411cee3ef3d9
eduardocastilho <eduardohlitsac@gmail.com>
2022-10-07 16:13:12 -0300

    Corrigido (Estados não apareciam no edit)

--- locadoradeveiculos/Controllers/ClientesController.cs
+++ locadoradeveiculos/Controllers/ClientesController.cs
@@ -87,6 +87,17 @@ namespace locadoradeveiculos.Controllers
             {
                 return NotFound();
             }
+
+            var estado = Enum.GetValues(typeof(Estado))
+                .Cast<Estado>()
+                .Select(e => new SelectListItem
+                {
+                    Value = e.ToString(),
+                    Text = e.ToString()
+                });
+
+            ViewBag.bagEstado = estado;
+
             return View(cliente);
 
         }
